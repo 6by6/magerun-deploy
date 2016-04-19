8bfcb1f5b91deb5548bb49ece91b76f4a01bca80
daniel <daniel@sixbysix.co.uk>
2016-04-19 12:26:18 +0100

    setting up modman structure

---
+++ src/config/common-deploy.rb
@@ -0,0 +1,31 @@
+require 'capistrano/ext/multistage'
+
+set :stage_dir,   "app/config/deploy"
+set :stages,      %w()
+set :default_stage, ""
+
+set :application, ""
+set :repository,  ""
+set :scm,         :git
+
+set  :keep_releases,  3
+after "deploy:update", "deploy:cleanup"
+
+default_run_options[:pty] = true
+set :use_sudo, false
+
+set :app_symlinks, ["/media", "/var", "/sitemaps", "/staging", "/bower_components", "/node_modules", "/wp/wp-content/uploads"]
+set :app_shared_dirs, ["/app/etc", "/sitemaps", "/media", "/var", "/staging", "/bower_components", "/node_modules", "/wp/wp-content/uploads"]
+set :app_shared_files, ["/app/etc/local.xml", "/robots.txt", "/wp/wp-config.php"]
+
+after "deploy:update_code" do
+  run("cd #{release_path}; composer update")
+
+  run("mkdir -p #{release_path}/skin/frontend/bluebellgray/default/build/js")
+  run("mkdir -p #{release_path}/skin/frontend/bluebellgray/default/build/css")
+  run_locally("npm install --silent")
+  run_locally("bower install --config.interactive=false")
+  run_locally("gulp release")
+  upload("skin/frontend/bluebellgray/default/build/js", "#{release_path}/skin/frontend/bluebellgray/default/build/js")
+  upload("skin/frontend/bluebellgray/default/build/css", "#{release_path}/skin/frontend/bluebellgray/default/build/css")
+end
